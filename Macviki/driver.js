5e17da9a003b28beb2a9120c747c791bb890d45c
Waitsnake <942085+Waitsnake@users.noreply.github.com>
2025-10-04 02:38:13 +0200

    Update driver.js

--- Macviki/driver.js
+++ Macviki/driver.js
@@ -1,8 +1,6 @@
 ;(function() {
     'use strict'
 
-    // INTERCEPT CONSOLE.LOG
-
     const oldLog = console.log
     console.log = (...args) => {
         const message = args.map((x) => String(x)).join(' ')
@@ -10,46 +8,111 @@
         oldLog.apply(console, args)
     }
 
-    window.macflix = mountDriver()
+    window.macviki = mountDriver()
 
     function mountDriver() {
-        if (window.macflix) {
+        if (window.macviki) {
             console.log('already mounted')
-            return window.macflix
+            return window.macviki
         } else {
             console.log('mounting...')
         }
-        
-        ////////////////////////////////////////////////////////////
 
-        // workaround: try install once per each sec (was not working on url change event?)
-        setInterval(function() {
-            // we only do changes once we on the videos page
-            if (location.pathname.startsWith('/videos/')) {
-                // REMOVE ANNOYING VIDEO PAUSE OVERLAY
-                document.getElementsByClassName("vmp-pause-overlay")[0]?.remove();
-                
-                // DETECT ONCLICK ON FULLSCREEN BUTTON
-                document.getElementsByClassName("vjs-icon-fullscreen-enter")[0]?.setAttribute('onclick','window.webkit.messageHandlers.requestFullscreen.postMessage(null)')
+        let playerObserver = null
+        
+        // remove rating display
+        const style = document.createElement('style')
+        style.id = 'macviki-age-hide'
+        style.innerHTML = `
+          .sc-17jko1v-0,
+          .sc-akvsl4-1,
+          .sc-akvsl4-0 {
+            display: none !important;
+          }
+        `
+        document.head.appendChild(style)
+
+
+        function handleWatchMode() {
+            console.log("Activating watch mode for Viki")
+
+            // Pause-Overlay dauerhaft killen via CSS
+            if (!document.getElementById('macviki-hide-overlays')) {
+                const style = document.createElement('style')
+                style.id = 'macviki-hide-overlays'
+                style.innerHTML = `
+                  .vmp-pause-overlay {
+                    display: none !important;
+                  }
+                `
+                document.head.appendChild(style)
             }
-        }, 1000);
-        
-        ////////////////////////////////////////////////////////////
-        
-        // some helper query functions (not used at the moment)
 
-        const qs = (...args) => document.querySelector(...args)
-        const qsa = (...args) => document.querySelectorAll(...args)
+            // Falls alter Observer noch läuft, erst stoppen
+            if (playerObserver) {
+                playerObserver.disconnect()
+                playerObserver = null
+            }
 
-        ////////////////////////////////////////////////////////////
+            // Neuen Observer starten
+            playerObserver = new MutationObserver(mutations => {
+                for (const m of mutations) {
+                    for (const node of m.addedNodes) {
+                        if (node.nodeType === 1) {
+                            // Overlay sicherheitshalber ausblenden
+                            if (node.classList?.contains("vmp-pause-overlay")) {
+                                node.style.display = "none"
+                            }
+                            // Fullscreen Button patchen
+                            if (node.classList?.contains("vjs-icon-fullscreen-enter")) {
+                                node.setAttribute(
+                                    'onclick',
+                                    'window.webkit.messageHandlers.requestFullscreen.postMessage(null)'
+                                )
+                            }
+                            // Falls der Button im neuen DOM-Baum steckt
+                            const fs = node.querySelector?.(".vjs-icon-fullscreen-enter")
+                            if (fs) {
+                                fs.setAttribute(
+                                    'onclick',
+                                    'window.webkit.messageHandlers.requestFullscreen.postMessage(null)'
+                                )
+                            }
+                        }
+                    }
+                }
+            })
+            playerObserver.observe(document.body, { childList: true, subtree: true })
 
-        // DETECT URL CHANGE
-        
+            // Sofort initial patch versuchen
+            const fsButton = document.querySelector(".vjs-icon-fullscreen-enter")
+            if (fsButton) {
+                fsButton.setAttribute(
+                    'onclick',
+                    'window.webkit.messageHandlers.requestFullscreen.postMessage(null)'
+                )
+            }
+        }
+
         function handleUrlChange(path) {
             const message = { url: path }
-            console.log('handleUrlChange ', path)
+            console.log('handleUrlChange', path)
             window.webkit.messageHandlers.onPushState.postMessage(message)
+
+            if (path.startsWith('/videos/')) {
+                handleWatchMode()
+            } else {
+                // raus aus Video → Observer stoppen
+                if (playerObserver) {
+                    playerObserver.disconnect()
+                    playerObserver = null
+                }
+            }
         }
+
+        // SPA Navigation abfangen
+        history.onpopstate = () => handleUrlChange(location.pathname)
+        window.onpopstate = () => handleUrlChange(location.pathname)
 
         const pushState = history.pushState
         history.pushState = (...args) => {
@@ -63,3 +126,4 @@
         }
     }
 })()
+
